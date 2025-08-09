2b175d56d84088301df4f1758e96cd6aa58ff3d1
Gregory Gilbert <gergtreblig@hotmail.com>
2025-08-08 18:00:13 -0700

    Sorting practice

---
+++ main.py
@@ -0,0 +1,48 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    middle = len(array) // 2
+    left = merge_sort(array[:middle])
+    right = merge_sort(array[middle:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    left_index, right_index = 0, 0
+    result = []
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(bubble_sort([5, 4, 3, 2, 1]))
+    print(bubble_sort([5, 24, 30, 2, 12]))
+    print(merge_sort([5, 4, 3, 2, 1]))
+    print(merge_sort([5, 24, 30, 12, 2]))
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
