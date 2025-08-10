0a2cb0c4dd772219877c5314f3299adba01f145c
Gregory Gilbert <gergtreblig@hotmail.com>
2025-08-09 23:20:06 -0700

    Adding custom fibonnaci sequence

--- fibonacci.py
+++ fibonacci.py
@@ -8,8 +8,23 @@ def fibonacci(fibi):
     else:
         return fibonacci(fibi - 1) + fibonacci(fibi - 2)
 
+def fibonacci_custom(first, second, length):
+    hold = 0
+    print(first, second)
+    for i in range(1, length-2):
+        first, second = second, first + second
+        print(second)
+
+
 if __name__ == '__main__':
     fibi = 1
     while fibi != 0:
         fibi = int(input('What number of fibonacci do you want? '))
         print("Fibonacci number " + str(fibi) + " is:", fibonacci(fibi))
+
+    print("What numbers to start with?")
+    first = int(input("First number: "))
+    second = int(input("Second number: "))
+    length = int(input("What length of sequence do you want: "))
+    fibonacci_custom(first, second, length)
+
